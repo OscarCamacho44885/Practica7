2dcfb8190fcd2cb1fd5e3784d07f8a07a8731391
OscarCamacho44885 <oscar.camacho94@unach.mx>
2024-09-20 21:57:49 -0600

    Practica 7
    Una pagina web de quesadillas

---
+++ practica7/index2.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<body>
+    <?php 
+    echo 'Hola PHP';
+    echo '<h1>Hola php</h1>';
+    echo '<hr>';
+    echo '<br>';
+    //comentario
+    $miNombre= 'Oscar Daniel Camacho Melgar';
+    echo 'Mi nombre es: '.$miNombre;
+    ?>
+</body>
+</html>
